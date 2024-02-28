6e5a3b310ee41887ab8500ede31763804de47b81
zeevo <8338013+zeevo@users.noreply.github.com>
2024-02-28 18:52:56 -0500

    remove migrate.ts

--- src/migrate.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { db, connection } from "./server/db";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-
-// This will run migrations on the database, skipping the ones already applied
-await migrate(db, { migrationsFolder: "./drizzle" });
-
-// Don't forget to close the connection, otherwise the script will hang
-await connection.end();
